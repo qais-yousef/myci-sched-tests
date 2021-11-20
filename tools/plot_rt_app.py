c74bdf7889579dc3b2cfcd2dd3b3a3d5ef52e195
Qais Yousef <qyousef@layalina.io>
2021-11-20 01:42:27 +0000

    plot_rt_app: update to plot data from perfetto
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- tools/plot_rt_app.py
+++ tools/plot_rt_app.py
@@ -1,20 +1,64 @@
 #!/usr/bin/env python3
 import pandas as pd
 from tabulate import tabulate
+from perfetto.trace_processor import TraceProcessor as tp
 import os
 
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-df_all = pd.DataFrame()
+query_freq = "select ts, cpu, value as freq from counter join cpu_counter_track on counter.track_id = cpu_counter_track.id"
+query_util = "select ts, EXTRACT_ARG(arg_set_id, 'comm'), EXTRACT_ARG(arg_set_id, 'util') from raw where name = 'sched_pelt_se'"
 
 for file in sorted(os.listdir()):
     if file.endswith(".csv"):
-        df = pd.read_csv(file)
-        df_all[file] = df['slack']
+        file = file.replace('.csv', '')
+        df_result = pd.read_csv(file + '.csv')
 
-print(tabulate(df_all.describe(), headers='keys', tablefmt='psql'))
+        print(tabulate(df_result.describe(), headers='keys', tablefmt='psql'))
 
-df_all.plot(figsize=(16,8), ylim=(-40000, 20000), style='o-').get_figure().savefig("result.png")
-df_all.plot.hist(figsize=(16,8), bins=100, alpha=1, xlim=(-40000, 20000)).get_figure().savefig("result_hist.png")
-df_all.plot.box(figsize=(16,8), ylim=(-40000, 20000)).get_figure().savefig("result_box.png")
+        trace = tp(file_path=file + '.perfetto-trace')
+        trace_freq = trace.query(query_freq)
+        trace_util = trace.query(query_util)
+        df_freq = trace_freq.as_pandas_dataframe()
+        df_util = trace_util.as_pandas_dataframe()
+
+        plt.figure(figsize=(16,8))
+        plt.subplot(4, 1, 1)
+        df_result.slack.plot(ylim=(0, 20000), style='o-', title='slack', xlim=(df_result.index[0], df_result.index[-1]))
+        plt.subplot(4, 2, 7)
+        df_result.slack.plot.hist(bins=100, alpha=1, title='slack hist')
+        plt.subplot(4, 2, 8)
+        df_result.slack.plot.box(title='slack box')
+
+        try:
+            df_freq.ts = df_freq.ts - df_freq.ts[0]
+            df_freq.ts = df_freq.ts / 1000000000
+            df_freq.freq = df_freq.freq / 1000000
+            df_freq.set_index('ts', inplace=True)
+
+            df_freq = df_freq[df_freq.cpu == 0]
+
+            plt.subplot(4, 1, 2)
+            df_freq.freq.plot(title='frequency', alpha=0.75, drawstyle='steps-post', xlim=(df_util.index[0], df_util.index[-1]))
+        except:
+            pass
+
+        try:
+            df_util.ts = df_util.ts - df_util.ts[0]
+            df_util.ts = df_util.ts / 1000000000
+            df_util.set_index('ts', inplace=True)
+
+            df_util.columns = df_util.columns.str.replace('EXTRACT_ARG\(arg_set_id, \'', '')
+            df_util.columns = df_util.columns.str.replace('\'\)', '')
+            df_util = df_util[df_util.comm.str.contains('thread0')]
+
+            plt.subplot(4, 1, 3)
+            df_util.groupby('comm').util.plot(title='util', alpha=0.75, xlim=(df_util.index[0], df_util.index[-1]))
+            plt.axhline(y=100, color='r', linestyle='-')
+        except:
+            pass
+
+        plt.tight_layout()
+        plt.savefig('result.png')
