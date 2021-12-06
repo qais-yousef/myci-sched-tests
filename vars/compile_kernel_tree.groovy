21e25453429d2d602782679c093b74d47da026cc
Qais Yousef <qyousef@layalina.io>
2021-12-06 16:25:06 +0000

    sched-tesets-kernel-compile-test: Allow user to configure the test more
    We accept env variables to set uclamp_min, uclamp_max and number of
    parallel jobs.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/compile_kernel_tree.groovy
+++ vars/compile_kernel_tree.groovy
@@ -1,9 +1,10 @@
-def call(version, log) {
+def call(version, log, uclamp_min, uclamp_max, num_parallel_jobs) {
 	sh """
 		pushd linux
 		git checkout "${version}"
 		make defconfig
-		/usr/bin/time -o "../${log}" -p make -j 2
+		uclampset -m ${uclamp_min} -M ${uclamp_max} \
+			/usr/bin/time -o "../${log}" -p make -j ${num_parallel_jobs}
 		popd
 	"""
 }
