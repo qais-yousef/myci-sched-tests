5f5124e092fe1be726c6cdde76a6381d37b3436b
Qais Yousef <qyousef@layalina.io>
2022-01-30 15:31:03 +0000

    perfetto: Support android devices
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/start_perfetto.groovy
+++ vars/start_perfetto.groovy
@@ -1,6 +1,35 @@
 def call(name) {
-	sh """
-		touch ${name}.perfetto-trace
-		tracebox -o ${name}.perfetto-trace --txt -d -c ./tools/config.pbtx &> perfetto.pid
-	"""
+	switch (env.MYCI_NODE_TYPE) {
+	case "android":
+		if (env.IPADDRESS && env.PORT) {
+			sh """
+				# Cleanup any potential leftover run..
+				adb -s ${IPADDRESS}:${PORT} shell -x "killall perfetto"
+				sleep 3
+
+				# Create our own directory to store perfetto-trace and clean any potential leftover
+				adb -s ${IPADDRESS}:${PORT} shell "mkdir -p /data/misc/perfetto-traces/myci/"
+				adb -s ${IPADDRESS}:${PORT} shell "rm -f /data/misc/perfetto-traces/myci/*.perfetto-trace"
+
+				cat ./tools/config.pbtx | adb -s ${IPADDRESS}:${PORT} shell \
+					perfetto -d -c - --txt -o /data/misc/perfetto-traces/myci/${name}.perfetto-trace
+
+			"""
+		} else {
+			error "Missing IPADDRESS and/or PORT info"
+		}
+		break
+	case "linux":
+		sh """
+			# Cleanup any potential leftover run..
+			kill -TERM `cat perfetto.pid` || true
+			sleep 3
+
+			touch ${name}.perfetto-trace
+			tracebox -o ${name}.perfetto-trace --txt -d -c ./tools/config.pbtx &> perfetto.pid
+		"""
+		break
+	default:
+		break
+	}
 }
--- vars/stop_perfetto.groovy
+++ vars/stop_perfetto.groovy
@@ -1,5 +1,55 @@
 def call() {
-	sh """
-		kill -TERM `cat perfetto.pid`
-	"""
+	switch (env.MYCI_NODE_TYPE) {
+	case "android":
+		if (env.IPADDRESS && env.PORT) {
+			sh """
+				#
+				# Kill perfetto process to dump the trace
+				#
+				adb -s ${IPADDRESS}:${PORT} shell -x "killall perfetto"
+
+				#
+				# Make sure trace dump has finished before pulling the file
+				#
+				TRACE_F=`adb -s ${IPADDRESS}:${PORT} shell "find /data/misc/perfetto-traces/myci -name '*.perfetto-trace'"`
+
+				size_a=0
+				size_b=0
+				while true
+				do
+					size_a=`adb -s ${IPADDRESS}:${PORT} shell "ls -l \$TRACE_F" | awk '{print \$5}'`
+
+					if [ \$size_a -eq \$size_b ]; then
+						break
+					fi
+
+					if [ \$size_b -ne 0 ]; then
+						sleep 3
+					fi
+
+					size_b=\$size_a
+				done
+
+				#
+				# We are ready, pull!
+				#
+				adb -s ${IPADDRESS}:${PORT} pull \$TRACE_F
+
+				#
+				# Don't leave leftovers..
+				#
+				adb -s ${IPADDRESS}:${PORT} shell rm \$TRACE_F
+			"""
+		} else {
+			error "Missing IPADDRESS and/or PORT info"
+		}
+		break
+	case "linux":
+		sh """
+			kill -TERM `cat perfetto.pid`
+		"""
+		break
+	default:
+		break
+	}
 }
