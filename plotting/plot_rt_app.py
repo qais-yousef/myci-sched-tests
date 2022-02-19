9d3ab4f04293bd954506bf47700065e3461749e8
Qais Yousef <qyousef@layalina.io>
2022-02-19 21:35:54 +0000

    plot_rt_app: Use the new runtime module
    Now we have a modular plotting mechanisms! We need to build on top.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/plot_rt_app.py
+++ plotting/plot_rt_app.py
@@ -4,13 +4,13 @@ from tabulate import tabulate
 from perfetto.trace_processor import TraceProcessor as tp
 import freq
 import util
+import runtime
 import os
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-query_runtime = "select ts, EXTRACT_ARG(arg_set_id, 'comm'), EXTRACT_ARG(arg_set_id, 'runtime'), EXTRACT_ARG(arg_set_id, 'vruntime') from raw where name = 'sched_stat_runtime'"
 
 num_rows=7
 
@@ -24,10 +24,9 @@ for file in sorted(os.listdir()):
         trace = tp(file_path=file + '.perfetto-trace')
         freq.init(trace)
         util.init(trace)
-        trace_runtime = trace.query(query_runtime)
+        runtime.init(trace)
         trace.close()
 
-        df_runtime = trace_runtime.as_pandas_dataframe()
 
         plt.figure(figsize=(16,16))
         row_pos = 1
@@ -45,27 +44,7 @@ for file in sorted(os.listdir()):
         df_result.slack.plot.box(title='slack box')
         row_pos += 1
 
-        try:
-            df_runtime.ts = df_runtime.ts - df_runtime.ts[0]
-            df_runtime.ts = df_runtime.ts / 1000000000
-            df_runtime.set_index('ts', inplace=True)
-
-            df_runtime.columns = df_runtime.columns.str.replace('EXTRACT_ARG\(arg_set_id, \'', '', regex=True)
-            df_runtime.columns = df_runtime.columns.str.replace('\'\)', '', regex=True)
-            df_runtime = df_runtime[df_runtime.comm.str.contains('thread0')]
-
-            df_runtime.runtime = df_runtime.runtime / 1000000
-            df_runtime.vruntime = df_runtime.vruntime / 1000000
-
-            plt.subplot(num_rows, 1, row_pos)
-            row_pos += 1
-            df_runtime.groupby('comm').runtime.plot(title='runtime (ms)', alpha=0.75, xlim=(df_runtime.index[0], df_runtime.index[-1]))
-            plt.subplot(num_rows, 1, row_pos)
-            row_pos += 1
-            df_runtime.groupby('comm').vruntime.plot(title='vruntime (ms)', alpha=0.75, xlim=(df_runtime.index[0], df_runtime.index[-1]))
-        except:
-            print("Error plotting runtime")
-            pass
+        row_pos = runtime.plot(num_rows=num_rows, row_pos=row_pos, threads=['thread0'])
 
         plt.tight_layout()
         plt.savefig(file + '.png')
