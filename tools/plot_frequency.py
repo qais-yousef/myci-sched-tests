8671d49c077532810bdd34e52cdce46c7a8050ad
Qais Yousef <qyousef@layalina.io>
2022-02-19 01:56:41 +0000

    plot_frequency: Only print clusters
    The code was heavily refactored too. And the query was fixed as it
    didn't work against newest captures from Android devices. We need to be
    specific which column we want from the database.
    
    With new changes, we don't unnecessarily plot other cpus in the same
    cluster as they'll be identical.
    
    We still have issues in specifying the correct figure size in case there
    were too many clusters or a large number of cpus in case they aren't
    clustered.
    
    Also fix SettingWithCopyWarning.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- tools/plot_frequency.py
+++ tools/plot_frequency.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import pandas as pd
+import numpy as np
 from perfetto.trace_processor import TraceProcessor as tp
 import os
 
@@ -7,17 +8,12 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-query_freq = "select ts, cpu, value as freq from counter join cpu_counter_track on counter.track_id = cpu_counter_track.id"
+query = "select ts, cpu, value as freq from counter join cpu_counter_track on counter.track_id = cpu_counter_track.id where t.name = 'cpufreq'"
+query = "select ts, cpu, value as freq from counter as c left join cpu_counter_track as t on c.track_id = t.id where t.name = 'cpufreq'"
 
-for file in sorted(os.listdir()):
-    if file.endswith(".perfetto-trace"):
-        trace = tp(file_path=file)
-        trace_freq = trace.query(query_freq)
-        trace.close()
-
+def plot(trace, start_row=1, num_cols=1):
+        trace_freq = trace.query(query)
         df_freq = trace_freq.as_pandas_dataframe()
-
-        plt.figure(figsize=(16,16))
 
         try:
             df_freq.ts = df_freq.ts - df_freq.ts[0]
@@ -27,17 +23,39 @@ for file in sorted(os.listdir()):
             df_freq.set_index('ts', inplace=True)
 
             nr_cpus = len(df_freq.cpu.unique())
-            nr_freqs = len(df_freq.freq.unique())
 
+            clusters = [0]
+            df_freq_cpu = df_freq[df_freq.cpu == 0]
             for cpu in range(nr_cpus):
+                df_freq_next_cpu = df_freq[df_freq.cpu == cpu]
+
+                if np.array_equal(df_freq_cpu.freq.values, df_freq_next_cpu.freq.values):
+                    continue
+
                 df_freq_cpu = df_freq[df_freq.cpu == cpu]
-                plt.subplot(nr_cpus * 2 + 1, 1, cpu * 2 + 1)
-                df_freq_cpu.freq.plot(title='CPU' + str(cpu) + ' frequency', alpha=0.75, drawstyle='steps-post', xlim=(df_freq.index[0], df_freq.index[-1]))
-                plt.subplot(nr_cpus * 2 + 1, 1, cpu * 2 + 2)
+                clusters.append(cpu)
+
+            nr_cpus = len(clusters)
+            for cpu in clusters:
+                df_freq_cpu = df_freq[df_freq.cpu == cpu].copy()
                 df_freq_cpu['duration'] = -1 * df_freq_cpu._ts.diff(periods=-1)
+
+                plt.subplot(nr_cpus * 2, num_cols, start_row)
+                df_freq_cpu.freq.plot(title='CPU' + str(cpu) + ' frequency', alpha=0.75, drawstyle='steps-post', style='o-', xlim=(df_freq.index[0], df_freq.index[-1]))
+
+                plt.subplot(nr_cpus * 2, num_cols, start_row + 1)
                 df_freq_cpu.groupby('freq').duration.sum().plot.bar(title='Frequency residency', alpha=0.75)
+
+                start_row += 2
         except:
             pass
+
+for file in sorted(os.listdir()):
+    if file.endswith(".perfetto-trace"):
+        trace = tp(file_path=file)
+        plt.figure(figsize=(16,16))
+        plot(trace)
+        trace.close()
 
         plt.tight_layout()
         plt.savefig(file.replace('.perfetto-trace', '') + '_frequency.png')
