a24cb8cfcd383b86d7d80e9fac9de113711c719d
Qais Yousef <qyousef@layalina.io>
2022-02-19 21:12:38 +0000

    plot_rt_app: Use new freq module
    And be smarter about how we handle row_pos to manage all subplots.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/plot_rt_app.py
+++ plotting/plot_rt_app.py
@@ -2,40 +2,42 @@
 import pandas as pd
 from tabulate import tabulate
 from perfetto.trace_processor import TraceProcessor as tp
+import freq
 import os
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-query_freq = "select ts, cpu, value as freq from counter join cpu_counter_track on counter.track_id = cpu_counter_track.id"
 query_util = "select ts, EXTRACT_ARG(arg_set_id, 'comm'), EXTRACT_ARG(arg_set_id, 'util') from raw where name = 'sched_pelt_se'"
 query_runtime = "select ts, EXTRACT_ARG(arg_set_id, 'comm'), EXTRACT_ARG(arg_set_id, 'runtime'), EXTRACT_ARG(arg_set_id, 'vruntime') from raw where name = 'sched_stat_runtime'"
+
+num_rows=7
 
 for file in sorted(os.listdir()):
     if file.endswith(".csv"):
         file = file.replace('.csv', '')
         df_result = pd.read_csv(file + '.csv')
 
-        print(tabulate(df_result.describe(), headers='keys', tablefmt='psql'))
+        # print(tabulate(df_result.describe(), headers='keys', tablefmt='psql'))
 
         trace = tp(file_path=file + '.perfetto-trace')
-        trace_freq = trace.query(query_freq)
+        freq.init(trace)
         trace_util = trace.query(query_util)
         trace_runtime = trace.query(query_runtime)
         trace.close()
 
-        df_freq = trace_freq.as_pandas_dataframe()
         df_util = trace_util.as_pandas_dataframe()
         df_runtime = trace_runtime.as_pandas_dataframe()
 
-        plt.figure(figsize=(16,8))
-        plt.subplot(6, 1, 1)
+        plt.figure(figsize=(16,16))
+        row_pos = 1
+
+        plt.subplot(num_rows, 1, row_pos)
+        row_pos += 1
         df_result.slack.plot(ylim=(0, 20000), style='o-', title='slack', xlim=(df_result.index[0], df_result.index[-1]))
-        plt.subplot(6, 2, 7)
-        df_result.slack.plot.hist(bins=100, alpha=1, title='slack hist')
-        plt.subplot(6, 2, 8)
-        df_result.slack.plot.box(title='slack box')
+
+        row_pos = freq.plot(num_rows=num_rows, row_pos=row_pos, cpus=[0])
 
         try:
             df_util.ts = df_util.ts - df_util.ts[0]
@@ -46,24 +48,19 @@ for file in sorted(os.listdir()):
             df_util.columns = df_util.columns.str.replace('\'\)', '', regex=True)
             df_util = df_util[df_util.comm.str.contains('thread0')]
 
-            plt.subplot(6, 1, 3)
+            plt.subplot(num_rows, 1, row_pos)
+            row_pos += 1
             df_util.groupby('comm').util.plot(title='util', alpha=0.75, xlim=(df_util.index[0], df_util.index[-1]))
             plt.axhline(y=100, color='r', linestyle='-')
         except:
+            print("Error plotting util")
             pass
 
-        try:
-            df_freq.ts = df_freq.ts - df_freq.ts[0]
-            df_freq.ts = df_freq.ts / 1000000000
-            df_freq.freq = df_freq.freq / 1000000
-            df_freq.set_index('ts', inplace=True)
-
-            df_freq = df_freq[df_freq.cpu == 0]
-
-            plt.subplot(6, 1, 2)
-            df_freq.freq.plot(title='frequency', alpha=0.75, drawstyle='steps-post', xlim=(df_util.index[0], df_util.index[-1]))
-        except:
-            pass
+        plt.subplot(num_rows, 2, row_pos * 2 - 1)
+        df_result.slack.plot.hist(bins=100, alpha=1, title='slack hist')
+        plt.subplot(num_rows, 2, row_pos * 2 - 0)
+        df_result.slack.plot.box(title='slack box')
+        row_pos += 1
 
         try:
             df_runtime.ts = df_runtime.ts - df_runtime.ts[0]
@@ -77,11 +74,14 @@ for file in sorted(os.listdir()):
             df_runtime.runtime = df_runtime.runtime / 1000000
             df_runtime.vruntime = df_runtime.vruntime / 1000000
 
-            plt.subplot(6, 1, 5)
+            plt.subplot(num_rows, 1, row_pos)
+            row_pos += 1
             df_runtime.groupby('comm').runtime.plot(title='runtime (ms)', alpha=0.75, xlim=(df_runtime.index[0], df_runtime.index[-1]))
-            plt.subplot(6, 1, 6)
+            plt.subplot(num_rows, 1, row_pos)
+            row_pos += 1
             df_runtime.groupby('comm').vruntime.plot(title='vruntime (ms)', alpha=0.75, xlim=(df_runtime.index[0], df_runtime.index[-1]))
         except:
+            print("Error plotting runtime")
             pass
 
         plt.tight_layout()
