72029a54cb76edbd9f9f422cac32eb539bc51c38
Qais Yousef <qyousef@layalina.io>
2022-02-20 03:13:05 +0000

    plot/table: Add new subplot function
    To create a plot on its own subplot. Maybe we should swap the name with
    plot, hmmm..
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/table.py
+++ plotting/table.py
@@ -11,3 +11,19 @@ def plot(df, columns=[], height=0.3):
               rowLabels=df_desc.index, rowLoc='center',
               colLabels=df_desc.columns, cellLoc='center',
               bbox=[0, -(height+0.1), 1, height])
+
+def subplot(df, num_rows=1, row_pos=1, columns=[]):
+    if len(columns):
+        df = df.loc[:,columns]
+
+    plt.subplot(num_rows, 1, row_pos)
+    row_pos += 1
+    df_desc = df.describe(percentiles=[0.9, 0.95, 0.99]).applymap('{:,.2f}'.format)
+    plt.gca().table(cellText=df_desc.values,
+              rowLabels=df_desc.index, rowLoc='center',
+              colLabels=df_desc.columns, cellLoc='center',
+              loc='center')
+    plt.gca().axis('off')
+    plt.gca().axis('tight')
+
+    return row_pos
