726a40c1e6ab1d924efb3dc797dcaaea4dd93c7e
Qais Yousef <qyousef@layalina.io>
2022-02-20 02:21:30 +0000

    plot_wa_results: Refactor to put everything on one figure
    And use new table module to put the df.describe() on the figure too.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/plot_wa_results.py
+++ plotting/plot_wa_results.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import glob
 import sys
+import table
 
 target = sys.argv[1]
 workload = sys.argv[2]
@@ -10,20 +11,19 @@ workload = sys.argv[2]
 df = pd.read_csv("wa_output/results.csv")
 
 metrics = df.metric.unique()
-for metric in metrics:
-    df_metric = df[df.metric == metric]
-    plt.plot(df_metric.iteration, df_metric.value, label=metric)
-    print("--", metric)
-    print(df_metric.value.describe(percentiles=[0.9, 0.95, 0.99]))
+num_rows = len(metrics)
+row_pos = 1
 
-plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
-plt.savefig("{}_{}_results.png".format(target, workload), bbox_inches="tight")
+plt.figure(figsize=(16,32))
 
 for metric in metrics:
     df_metric = df[df.metric == metric]
-    plt.figure()
-    plt.plot(df_metric.iteration, df_metric.value, label=metric)
-    plt.legend()
-    plt.ylabel(df_metric.units.unique())
-    plt.xlabel("Iteration")
-    plt.savefig("{}_{}_{}.png".format(target, workload, metric))
+    plt.subplot(num_rows, 1, row_pos)
+    row_pos += 1
+    plt.stem(df_metric.iteration, df_metric.value, label=metric)
+    table.plot(df_metric, columns=['value'])
+    # plt.gca().legend(loc='upper right')
+    plt.gca().set_title(metric)
+
+plt.tight_layout()
+plt.savefig("{}_{}_results.png".format(target, workload))
