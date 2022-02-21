632c4d7b01ddb8a2e0a742b43ebea654e5a5618a
Qais Yousef <qyousef@layalina.io>
2022-02-21 13:10:11 +0000

    plot_sysbench: Use line plot again
    The bar was too dense for high n umber of iterations. It's easier to see
    the trends with a line plot.
    
    Also remove the text annotation from the mean axhline. The calculation
    of the position wasn't right. We can see the actual values from the
    table, so remove them for now until we fix the text.plot() to better
    manage adding it at the right location.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/plot_sysbench.py
+++ plotting/plot_sysbench.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import os
 import table
-import text
 
 import matplotlib
 matplotlib.use('Agg')
@@ -16,15 +15,15 @@ for file in sorted(os.listdir()):
         df_all[file] = df['events_per_second']
 
 plt.figure(figsize=(16,16))
-df_all.plot.bar(ax=plt.gca())
-b, t = plt.gca().get_ylim()
-i = 0
+df_all.plot(ax=plt.gca(), style='o-')
+
 color = ['b', 'orange', 'g']
+i = 0
 for column in df_all.columns:
     mean = df_all[column].mean()
     plt.axhline(y=mean, color=color[i], linestyle='-')
-    text.plot(0.1, mean/t, 'Mean = {:,.2f}'.format(mean))
     i += 1
+
 table.plot(df_all)
 plt.tight_layout()
 plt.savefig("sysbench_result.png")
