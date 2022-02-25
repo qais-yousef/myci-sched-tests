8a5a2825a9900171946c32ea723398838282be80
Qais Yousef <qyousef@layalina.io>
2022-02-25 01:45:10 +0000

    plot/power: Plot battery charge drop in mAh
    As an extra indication on how much power was used.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/power.py
+++ plotting/power.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import text
 
+query_charge = "select ts, value as charge_uah from counter as c left join counter_track t on c.track_id = t.id where t.name like 'batt.charge_uah'"
 query_current = "select ts, value as current_ua from counter as c left join counter_track t on c.track_id = t.id where t.name = 'batt.current_ua'"
 query_power = "select ts, t.name as rail, value as energy from counter as c left join counter_track t on c.track_id = t.id where t.name like 'power.rails.cpu.%'"
 
@@ -11,8 +12,10 @@ df_power = None
 
 def init(trace):
 
+        global trace_charge
         global trace_current
         global trace_power
+        trace_charge = trace.query(query_charge)
         trace_current = trace.query(query_current)
         trace_power = trace.query(query_power)
 
@@ -23,13 +26,14 @@ def num_rows():
             df_power = trace_power.as_pandas_dataframe()
 
         if df_power.empty:
-            return 1
+            return 2
         else:
-            return len(df_power.rail.unique()) + 1
+            return len(df_power.rail.unique()) + 2
 
 def plot(num_rows=0, row_pos=1):
 
         df_current = trace_current.as_pandas_dataframe()
+        df_charge = trace_charge.as_pandas_dataframe()
 
         global df_power
         if df_power is None:
@@ -38,6 +42,23 @@ def plot(num_rows=0, row_pos=1):
         if not num_rows:
             func = globals()['num_rows']
             num_rows = func()
+
+        try:
+            df_charge.ts = df_charge.ts - df_charge.ts[0]
+            df_charge.ts = df_charge.ts / 1000000000
+            df_charge['_ts'] = df_charge.ts
+            df_charge.set_index('ts', inplace=True)
+
+            df_charge.charge_uah = (df_charge.charge_uah - df_charge.charge_uah[0]) / 1000
+
+            plt.subplot(num_rows, 1, row_pos)
+            row_pos += 1
+            df_charge.charge_uah.plot(title='Battery Charge Drop (mAh)', alpha=0.75, drawstyle='steps-post', style='o-', xlim=(df_charge.index[0], df_charge.index[-1]))
+        except Exception as e:
+            # Most likely the trace has no power info
+            # TODO: Better detect this
+            print("Error processing power.plot()::charge_uah", e)
+            pass
 
         try:
             df_current.ts = df_current.ts - df_current.ts[0]
