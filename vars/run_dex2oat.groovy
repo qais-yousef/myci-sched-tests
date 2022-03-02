32637b64127f5c0c6bfc62a88d0b7287dc181336
Qais Yousef <qyousef@layalina.io>
2022-03-02 21:39:05 +0000

    sched-tests-dex2oat: Add a test to force profile-based compilation
    Of all packages. See:
    
    	https://source.android.com/devices/tech/dalvik/jit-compiler#force-compilation-of-a-specific-package
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

---
+++ vars/run_dex2oat.groovy
@@ -0,0 +1,22 @@
+def call(background=false) {
+	switch (env.MYCI_NODE_TYPE) {
+	case "android":
+		if (env.IPADDRESS && env.PORT) {
+			sh """
+				if [ "${background}" == "false" ]; then
+					adb -s ${IPADDRESS}:${PORT} shell -x 'cmd package compile -m speed-profile -f -a'
+				else
+					adb -s ${IPADDRESS}:${PORT} shell -x nohup 'cmd package compile -m speed-profile -f -a' &
+				fi
+			"""
+		} else {
+			error "Missing IPADDRESS and/or PORT info"
+		}
+		break
+	case "linux":
+		error "No linux support yet"
+		break
+	default:
+		break
+	}
+}
