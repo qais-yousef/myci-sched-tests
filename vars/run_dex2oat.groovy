4bbff83412a243d3fc602e4323521525bd7ba998
Qais Yousef <qyousef@layalina.io>
2022-03-04 01:40:53 +0000

    run_dex2oat: Finally use the env.ITERATIONS and env.DELAY
    Follow what the user asks for.. :-)
    
    Also improve sched-tests-dex2oat to include the post { always {} } step
    to ensure we clean up in case something failed. It's hard to kill the
    command to force JIT compilation, at least let us wait to ensure we
    leave the device in a good known state.
    
    Fix the description in the config.xml since now we will NOT trigger
    dex2oat only once, we'll follow the number of iters and delay specified
    by the user.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/run_dex2oat.groovy
+++ vars/run_dex2oat.groovy
@@ -1,9 +1,16 @@
-def call() {
+def call(iterations, delay) {
 	switch (env.MYCI_NODE_TYPE) {
 	case "android":
 		if (env.IPADDRESS && env.PORT) {
 			sh """
-				adb -s ${IPADDRESS}:${PORT} shell -x 'cmd package compile -m speed-profile -f -a'
+				for i in \$(seq ${iterations})
+				do
+					echo "Iteration [\$i]"
+					echo "Waiting ${delay}s before starting..."
+					sleep ${delay}
+
+					adb -s ${IPADDRESS}:${PORT} shell -x 'cmd package compile -m speed-profile -f -a'
+				done
 			"""
 		} else {
 			error "Missing IPADDRESS and/or PORT info"
