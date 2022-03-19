6562c10a9ea20261e10ce9b4aa8f0f86d752e174
Qais Yousef <qyousef@layalina.io>
2022-03-19 21:52:52 +0000

    vars/verify_params: Cleaner way to report params has changed
    Instead of exiting on error, interrupt with abort status and print more
    helpful messages.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/verify_params.groovy
+++ vars/verify_params.groovy
@@ -1,12 +1,20 @@
 def call(extra_checks=false) {
 	if (!env.NODE || extra_checks) {
-		error('Parameters has changed, please refresh the page and try again')
+		script {
+			currentBuild.description = "${NODE}: Parameters has changed, please refresh the page and try again"
+			currentBuild.result = 'ABORTED'
+			error('Aborting the build, params has changed. Please refresh the page and try again.')
+		}
 	}
 
 	switch (env.MYCI_NODE_TYPE) {
 	case "android":
 		if (!env.ITERATIONS || !env.DELAY || !env.RUN_DEX2OAT) {
-			error('Parameters has changed, please refresh the page and try again')
+			script {
+				currentBuild.description = "${NODE}: Parameters has changed, please refresh the page and try again"
+				currentBuild.result = 'ABORTED'
+				error('Aborting the build, params has changed. Please refresh the page and try again.')
+			}
 		}
 		break
 	case "linux":
