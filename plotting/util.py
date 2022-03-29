2bd9093c070c3bca619f7ee59ba3a6506dbea2da
Qais Yousef <qyousef@layalina.io>
2022-03-29 18:36:42 +0100

    plot/util: Overlay overutilized info over cpu plots
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/util.py
+++ plotting/util.py
@@ -16,14 +16,22 @@ query_cfs = "select ts, \
             EXTRACT_ARG(arg_set_id, 'util') as util \
             from raw where name = 'sched_pelt_cfs'"
 
+query_ou = "select ts, \
+            EXTRACT_ARG(arg_set_id, 'overutilized') as overutilized, \
+            EXTRACT_ARG(arg_set_id, 'span') as span \
+            from raw where name = 'sched_overutilized'"
+
+df_ou = None
 df_util_cfs = None
 
 def init(trace):
 
         global trace_util_se
         global trace_util_cfs
+        global trace_ou
         trace_util_se = trace.query(query_se)
         trace_util_cfs = trace.query(query_cfs)
+        trace_ou = trace.query(query_ou)
 
 def num_rows(threads=[]):
 
@@ -33,6 +41,27 @@ def num_rows(threads=[]):
 
         # User must multiple this with len(threads) passed to plot()
         return 4 * len(threads) + len(df_util_cfs.cpu.unique()) * 2
+
+def overlay_ou():
+
+        global df_ou
+        if df_ou is None:
+            df_ou = trace_ou.as_pandas_dataframe()
+            df_ou.ts = df_ou.ts - df_ou.ts[0]
+            df_ou.ts = df_ou.ts / 1000000000
+            df_ou.set_index('ts', inplace=True)
+
+        start_ts = None
+        i = 0
+        for ou in df_ou.overutilized:
+            if ou:
+                if start_ts is None:
+                    start_ts = df_ou.index[i]
+            else:
+                if start_ts is not None:
+                    plt.gca().axvspan(start_ts, df_ou.index[i], alpha=0.1, color='r')
+                    start_ts = None
+            i += 1
 
 def plot(num_rows=0, row_pos=1, threads=[]):
 
@@ -58,11 +87,13 @@ def plot(num_rows=0, row_pos=1, threads=[]):
                 row_pos += 1
                 df = df_util_cfs_root[df_util_cfs_root.cpu == cpu]
                 df.util.plot(title='CPU {} util'.format(cpu), drawstyle='steps-post', alpha=0.75, legend=True, xlim=(df_util_cfs.index[0], df_util_cfs.index[-1]))
+                overlay_ou()
 
                 plt.subplot(num_rows, 1, row_pos)
                 row_pos += 1
                 df = df_util_cfs_others[df_util_cfs_others.cpu == cpu]
                 df.groupby('path').util.plot(title='CPU {} taskgroup util'.format(cpu), drawstyle='steps-post', alpha=0.75, legend=True, xlim=(df_util_cfs.index[0], df_util_cfs.index[-1]))
+                overlay_ou()
         except Exception as e:
             # Most likely the trace has no util info
             # TODO: Better detect this
