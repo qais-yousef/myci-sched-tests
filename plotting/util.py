838bcc9dbac68bedd24d5cc8477159534041c89d
Qais Yousef <qyousef@layalina.io>
2022-03-29 13:33:52 +0100

    plot/util: Add cpu and path fields
    To plot per cpu and per taskgroup distribution.
    
    Also fix a bug where we were overriding df_util when looping through the
    threads. If multiple threads were passed, we would get bogus results in
    every thread except the first one.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/util.py
+++ plotting/util.py
@@ -2,7 +2,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-query = "select ts, EXTRACT_ARG(arg_set_id, 'comm') as comm, EXTRACT_ARG(arg_set_id, 'pid') as pid, EXTRACT_ARG(arg_set_id, 'util') as util from raw where name = 'sched_pelt_se'"
+query = "select ts, \
+            EXTRACT_ARG(arg_set_id, 'cpu') as cpu, \
+            EXTRACT_ARG(arg_set_id, 'path') as path, \
+            EXTRACT_ARG(arg_set_id, 'comm') as comm, \
+            EXTRACT_ARG(arg_set_id, 'pid') as pid, \
+            EXTRACT_ARG(arg_set_id, 'util') as util \
+            from raw where name = 'sched_pelt_se'"
 
 def init(trace):
 
@@ -12,7 +18,7 @@ def init(trace):
 def num_rows():
 
         # User must multiple this with len(threads) passed to plot()
-        return 2
+        return 4
 
 def plot(num_rows=0, row_pos=1, threads=[]):
 
@@ -32,17 +38,27 @@ def plot(num_rows=0, row_pos=1, threads=[]):
             df_util.set_index('ts', inplace=True)
 
             for thread in threads:
-                df_util = df_util[df_util.comm.str.contains(thread)]
+                df = df_util[df_util.comm.str.contains(thread)]
 
                 plt.subplot(num_rows, 1, row_pos)
                 row_pos += 1
-                df_util.groupby('pid').util.plot(title=thread + ' util', alpha=0.75, xlim=(df_util.index[0], df_util.index[-1]))
+                df.groupby('pid').util.plot(title=thread + ' per pid util', alpha=0.75, xlim=(df_util.index[0], df_util.index[-1]))
                 plt.grid()
 
                 plt.subplot(num_rows, 1, row_pos)
                 row_pos += 1
-                plt.title(thread + ' util Histogram')
-                df_util.groupby('pid').util.hist(bins=100, density=False, grid=True, alpha=0.5)
+                plt.title(thread + ' util per pid Histogram')
+                df.groupby('pid').util.hist(bins=100, density=False, grid=True, alpha=0.5)
+
+                plt.subplot(num_rows, 1, row_pos)
+                row_pos += 1
+                plt.title(thread + ' cpu per pid Histogram')
+                df.groupby('pid').cpu.hist(bins=100, density=False, grid=True, alpha=0.5)
+
+                plt.subplot(num_rows, 1, row_pos)
+                row_pos += 1
+                plt.title(thread + ' taskgroup per pid Histogram')
+                df.groupby('pid').path.hist(bins=100, density=False, grid=True, alpha=0.5)
         except Exception as e:
             # Most likely the trace has no util info
             # TODO: Better detect this
