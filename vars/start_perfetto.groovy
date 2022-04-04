qais-yousef/myci-sched-tests

commit 36439b9056bcacb78f6372e621d7356c547c9e45

Add the ability to optionally collecdt PELT data from tests.
Hardwired to dex2oat only for now.

Signed-off-by: Qais Yousef <qyousef@layalina.io>

vars/start_perfetto.groovy
@@ -11,7 +11,13 @@ def call(name, configs="") {
 				adb -s ${IPADDRESS}:${PORT} shell "mkdir -p /data/misc/perfetto-traces/myci/"
 				adb -s ${IPADDRESS}:${PORT} shell "rm -f /data/misc/perfetto-traces/myci/*.perfetto-trace"
 
-				for config in ${configs}
+				configs=${configs}
+
+				if [ "${COLLECT_PELT}" == "true" ]; then
+					configs="\$configs pelt"
+				fi
+
+				for config in \$configs
 				do
 					cat ./tools/config.pbtx.\$config >> ./tools/config.pbtx
 				done
@@ -30,7 +36,13 @@ def call(name, configs="") {
 			kill -TERM `cat perfetto.pid` || true
 			sleep 3
 
-			for config in ${configs}
+			configs=${configs}
+
+			if [ "${COLLECT_PELT}" == "true" ]; then
+				configs="\$configs pelt"
+			fi
+
+			for config in \$configs
 			do
 				cat ./tools/config.pbtx.\$config >> ./tools/config.pbtx
 			done

plotting/plot_pelt.py
@@ -1,0 +1,25 @@
+#!/usr/bin/env python3
+from perfetto.trace_processor import TraceProcessor as tp
+import util
+import os
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+try:
+    threads = sys.argv[1].split()
+except:
+    threads = []
+
+for file in sorted(os.listdir()):
+    if file.endswith(".perfetto-trace"):
+        trace = tp(file_path=file)
+        util.init(trace)
+        trace.close()
+
+        plt.figure(figsize=(16,16))
+        util.plot(threads=threads)
+        plt.tight_layout()
+        plt.savefig(file.replace('.perfetto-trace', '') + '_util.png')

plotting/util.py
@@ -22,6 +22,10 @@ def plot(num_rows=0, row_pos=1, threads=[]):
             print("Error: must specify threads in util.plot()")
             return row_pos
 
+        if not num_rows:
+            func = globals()['num_rows']
+            num_rows = func() * len(threads)
+
         try:
             df_util.ts = df_util.ts - df_util.ts[0]
             df_util.ts = df_util.ts / 1000000000

sched-tests-dex2oat.jenkinsfile
@@ -4,6 +4,7 @@ pipeline {
 	parameters {
 		string(name: 'NODE', defaultValue: '', description: 'The DUT to run on. Must be Android based DUT.')
 		string(name: 'DESCRIPTION', defaultValue: '', description: 'Describe your experiment so that we have a meaningful name for this run to return to')
+		booleanParam(name: 'COLLECT_PELT', defaultValue: false, description: 'Collect PELT data and produce plots')
 		string(name: 'BRANCH', defaultValue: "${MYCI_BRANCH}", description: 'Which branch to run from? For power users experimenting/developing new changes.')
 		string(name: 'ITERATIONS', defaultValue: '10', description: 'Number of iterations to run')
 		string(name: 'DELAY', defaultValue: '10', description: 'Time in seconds to wait between each run.')
@@ -18,7 +19,7 @@ pipeline {
 		stage('Init') {
 			steps {
 				library "sched-tests-utils@${MYCI_BRANCH}"
-				verify_params()
+				verify_params(!env.COLLECT_PELT)
 				set_description()
 			}
 		}
@@ -48,6 +49,7 @@ pipeline {
 				plot_idle()
 				plot_power()
 				plot_jank()
+				plot_pelt(env.COLLECT_PELT == 'true', "dex2oat")
 			}
 		}
 		stage('Archive') {

tools/config.pbtx.pelt
@@ -5,6 +5,7 @@ data_sources: {
 
 			ftrace_events: "sched/sched_pelt_se"
 			ftrace_events: "sched/sched_pelt_cfs"
+			ftrace_events: "sched/sched_overutilized"
 
 			buffer_size_kb: 16384
 			drain_period_ms: 1000

vars/plot_pelt.groovy
@@ -1,0 +1,7 @@
+def call(condition=true, threads="") {
+	if (condition) {
+		sh """
+			plotting/plot_pelt.py "${threads}"
+		"""
+	}
+}

commit 0d92759ef8bb3acc345aa461608df42eb662250a

start_perfetto: Fix error when ${configs} is not empty
Was broken by commit:

	36439b9 ("Add the ability to optionally collect PELT data from tests.")

We need to wrap ${configs} with quotes otherwise jenkins would think
we're trying to execute a command.

Signed-off-by: Qais Yousef <qyousef@layalina.io>

vars/start_perfetto.groovy
@@ -19,7 +19,7 @@ def call(name, configs="") {
 				adb -s ${IPADDRESS}:${PORT} shell "mkdir -p /data/misc/perfetto-traces/myci/"
 				adb -s ${IPADDRESS}:${PORT} shell "rm -f /data/misc/perfetto-traces/myci/*.perfetto-trace"
 
-				configs=${configs}
+				configs="${configs}"
 
 				if [ "${COLLECT_PELT}" == "true" ]; then
 					configs="\$configs pelt"
@@ -48,7 +48,7 @@ def call(name, configs="") {
 			kill -TERM `cat perfetto.pid` || true
 			sleep 3
 
-			configs=${configs}
+			configs="${configs}"
 
 			if [ "${COLLECT_PELT}" == "true" ]; then
 				configs="\$configs pelt"