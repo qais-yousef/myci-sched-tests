f9f365c3f7bfafb486c014ecea33e124482eeb5f
Qais Yousef <qyousef@layalina.io>
2022-04-25 15:54:22 +0100

    sched-tests-profile-android: Revive this test
    The goal is to run multiple android tests and collect all the data in
    one go rather than manually trigger them all. We should end up with
    a report.
    
    We setup the infrastructure in this commit.
    
    We have a new deps on Copy Artifact Plugin. Parent 'myci' project
    should provide this for us.
    
    We don't archive perfetto-traces or tar files, only pictures. We should
    improve on this to reach the ultimate goal of having a final report
    produce to examine.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

---
+++ vars/build_android_job.groovy
@@ -0,0 +1,16 @@
+def call(test_name, quiet_period, run_dex2oat) {
+	test =	build quietPeriod: quiet_period, job: test_name, \
+		parameters: [string(name: 'NODE', value: params.NODE), \
+			     string(name: 'DESCRIPTION', value: params.DESCRIPTION), \
+			     string(name: 'BRANCH', value: params.BRANCH), \
+			     booleanParam(name: 'RUN_DEX2OAT', value: run_dex2oat), \
+			     string(name: 'ITERATIONS', value: params.ITERATIONS), \
+			     string(name: 'DELAY', value: params.DELAY)]
+
+	if (test.result == "SUCCESS") {
+		dir = test_name
+		if (run_dex2oat)
+			dir += '-dex2oat'
+		copyArtifacts filter: '*.png', fingerprintArtifacts: true, projectName: test_name, selector: specific(test.getNumber().toString()), target: dir
+	}
+}
