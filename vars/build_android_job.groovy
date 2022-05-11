49f98f9941fe7de378eea33af53d15d8295f045a
Qais Yousef <qyousef@layalina.io>
2022-05-11 13:49:35 +0100

    build_android_job: Wire up COLLECT{PELT, UCLAMP}
    Fix a bug where setting up the collection in sched-tests-profile-android
    didn't trigger UCLAMP and PELT collection.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/build_android_job.groovy
+++ vars/build_android_job.groovy
@@ -2,6 +2,8 @@ def call(test_name, quiet_period, run_dex2oat) {
 	test =	build quietPeriod: quiet_period, job: test_name, \
 		parameters: [string(name: 'NODE', value: params.NODE), \
 			     string(name: 'DESCRIPTION', value: params.DESCRIPTION), \
+			     booleanParam(name: 'COLLECT_PELT', value: params.COLLECT_PELT), \
+			     booleanParam(name: 'COLLECT_UCLAMP', value: params.COLLECT_UCLAMP), \
 			     string(name: 'BRANCH', value: params.BRANCH), \
 			     booleanParam(name: 'RUN_DEX2OAT', value: run_dex2oat), \
 			     string(name: 'ITERATIONS', value: params.ITERATIONS), \
