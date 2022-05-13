ca103b17ee2c87fd0a384bda1c0d4b08de7721a6
Qais Yousef <qyousef@layalina.io>
2022-05-13 23:22:30 +0100

    plot/uclamp: Plot uclamp_{min, max} residency
    Ignoring anything less than 1% residency to reduce the output noise.
    
    For threads we plot the histogram is it's a bit harder to calculate
    residency.
    
    Should give a good indication how long we spent in each value.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- plotting/uclamp.py
+++ plotting/uclamp.py
@@ -37,6 +37,7 @@ def __init():
             df_uclamp_cfs = trace_uclamp_cfs.as_pandas_dataframe()
             df_uclamp_cfs.ts = df_uclamp_cfs.ts - df_uclamp_cfs.ts[0]
             df_uclamp_cfs.ts = df_uclamp_cfs.ts / 1000000000
+            df_uclamp_cfs['_ts'] = df_uclamp_cfs.ts
             df_uclamp_cfs.set_index('ts', inplace=True)
 
         global df_uclamp_se
@@ -44,6 +45,7 @@ def __init():
             df_uclamp_se = trace_uclamp_se.as_pandas_dataframe()
             df_uclamp_se.ts = df_uclamp_se.ts - df_uclamp_se.ts[0]
             df_uclamp_se.ts = df_uclamp_se.ts / 1000000000
+            df_uclamp_se['_ts'] = df_uclamp_se.ts
             df_uclamp_se.set_index('ts', inplace=True)
 
 def num_rows(threads=[]):
@@ -51,7 +53,7 @@ def num_rows(threads=[]):
         __init()
 
         # User must multiple this with len(threads) passed to plot()
-        return 4 * len(threads) + len(df_uclamp_cfs.cpu.unique()) * 2
+        return 6 * len(threads) + len(df_uclamp_cfs.cpu.unique()) * 4
 
 def save_csv(prefix):
 
@@ -70,7 +72,10 @@ def plot(num_rows=0, row_pos=1, threads=[]):
 
         try:
             for cpu in sorted(df_uclamp_cfs.cpu.unique()):
-                df = df_uclamp_cfs[df_uclamp_cfs.cpu == cpu]
+                df = df_uclamp_cfs[df_uclamp_cfs.cpu == cpu].copy()
+                df['duration'] = -1 * df._ts.diff(periods=-1)
+
+                total_duration = df.duration.sum()
 
                 plt.subplot(num_rows, 1, row_pos)
                 row_pos += 1
@@ -83,6 +88,22 @@ def plot(num_rows=0, row_pos=1, threads=[]):
                 df.uclamp_min.plot(label='uclamp_min', legend=True, drawstyle='steps-post', alpha=0.75, color='g', xlim=(df_uclamp_cfs.index[0], df_uclamp_cfs.index[-1]))
                 df.uclamp_max.plot(label='uclamp_max', legend=True, drawstyle='steps-post', alpha=0.75, color='r', xlim=(df_uclamp_cfs.index[0], df_uclamp_cfs.index[-1]))
                 plt.grid()
+
+                plt.subplot(num_rows, 1, row_pos)
+                row_pos += 1
+                df_duration = df.groupby('uclamp_min').duration.sum() * 100 / total_duration
+                df_duration = df_duration[df_duration > 1]
+                ax = df_duration.plot.bar(title='CPU ' + str(cpu) + ' uclamp_min residency %', alpha=0.75, color='g')
+                ax.bar_label(ax.containers[0])
+                plt.grid()
+
+                plt.subplot(num_rows, 1, row_pos)
+                row_pos += 1
+                df_duration = df.groupby('uclamp_max').duration.sum() * 100 / total_duration
+                df_duration = df_duration[df_duration > 1]
+                ax = df_duration.plot.bar(title='CPU ' + str(cpu) + ' uclamp_max residency %', alpha=0.75, color='r')
+                ax.bar_label(ax.containers[0])
+                plt.grid()
         except Exception as e:
             # Most likely the trace has no util info
             # TODO: Better detect this
@@ -91,7 +112,7 @@ def plot(num_rows=0, row_pos=1, threads=[]):
 
         try:
             for thread in threads:
-                df = df_uclamp_se[df_uclamp_se.comm.str.contains(thread)]
+                df = df_uclamp_se[df_uclamp_se.comm.str.contains(thread)].copy()
 
                 plt.subplot(num_rows, 1, row_pos)
                 row_pos += 1
@@ -101,15 +122,29 @@ def plot(num_rows=0, row_pos=1, threads=[]):
                 plt.subplot(num_rows, 1, row_pos)
                 row_pos += 1
                 df.groupby('comm').uclamp_avg.plot(title=thread + ' uclamp_avg', drawstyle='steps-post', alpha=0.75, legend=True, xlim=(df_uclamp_se.index[0], df_uclamp_se.index[-1]))
+                plt.grid()
 
                 plt.subplot(num_rows, 1, row_pos)
                 row_pos += 1
                 df.groupby('comm').uclamp_min.plot(title=thread + ' uclamp_min', drawstyle='steps-post', alpha=0.75, legend=True, xlim=(df_uclamp_se.index[0], df_uclamp_se.index[-1]))
+                plt.grid()
+
+                plt.subplot(num_rows, 1, row_pos)
+                row_pos += 1
+                plt.title(thread + ' uclamp_min Histogram')
+                df.groupby('comm').uclamp_min.hist(density=True, bins=100, alpha=0.75, legend=True)
 
                 plt.subplot(num_rows, 1, row_pos)
                 row_pos += 1
                 df.groupby('comm').uclamp_max.plot(title=thread + ' uclamp_max', drawstyle='steps-post', alpha=0.75, legend=True, xlim=(df_uclamp_se.index[0], df_uclamp_se.index[-1]))
+                plt.grid()
+
+                plt.subplot(num_rows, 1, row_pos)
+                row_pos += 1
+                plt.title(thread + ' uclamp_max Histogram')
+                df.groupby('comm').uclamp_max.hist(density=True, bins=100, alpha=0.75, legend=True)
         except Exception as e:
+
             # Most likely the trace has no util info
             # TODO: Better detect this
             print("Error processing uclamp.plot() for threads:", e)
