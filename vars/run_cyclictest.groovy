fee4c0f46f75a19e79eb62406441c76b20bda323
Qais Yousef <qyousef@layalina.io>
2022-10-14 10:02:36 +0100

    run_cyclictest: Remove --laptop option
    It produces worse latency results but saves power - this could skew some
    test results when testing for example impact of softirq delays.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/run_cyclictest.groovy
+++ vars/run_cyclictest.groovy
@@ -4,7 +4,7 @@ def call(priority, duration, interval, num_threads) {
 		if (env.IPADDRESS && env.PORT) {
 			sh """
 				file="/data/cyclictest.json"
-				adb -s ${IPADDRESS}:${PORT} shell "cyclictest -t ${num_threads} -p ${priority} -D ${duration} -i ${interval} --laptop --json=\$file"
+				adb -s ${IPADDRESS}:${PORT} shell "cyclictest -t ${num_threads} -p ${priority} -D ${duration} -i ${interval} --json=\$file"
 				adb -s ${IPADDRESS}:${PORT} pull \$file
 				adb -s ${IPADDRESS}:${PORT} shell "rm \$file"
 			"""
@@ -14,7 +14,7 @@ def call(priority, duration, interval, num_threads) {
 		break
 	case "linux":
 		sh """
-			cyclictest -t ${num_threads} -p ${priority} -D ${duration} -i ${interval} --laptop --json=cyclictest.json
+			cyclictest -t ${num_threads} -p ${priority} -D ${duration} -i ${interval} --json=cyclictest.json
 		"""
 		break
 	default:
